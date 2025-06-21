9dfb3e0ac583c7449ed01fd597f9c72856b950de
Sumanth Jonnalagadda <sumanthjonnalagadda@Sumanths-MacBook-Air.local>
2025-06-21 14:55:47 +0530

    Added few other pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Home } from './pages/home/home';
+import { Services } from './pages/services/services';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'services', component: Services },
+];
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'reppd';
+
+  constructor(private router: Router){}
+
+  mobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen = false;
+  }
+
+  navigateToAbout(){
+    this.router.navigate(['about'])
+  }
+
+  navigateToServices(){
+    this.router.navigate(['services'])
+  }
 }
